48535c2c7f2307e71caa585a1fcf14830fe7e908
Your Name <youremail@yourdomain.com>
2023-05-27 06:02:18 -0700

    variables/functions

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
+
+// edit option
+let editElement;
+let editFlag = false;
+let editID = '';
+
+// event listeners
+// submit form
+form.addEventListener('submit', addItem)
+// functions
+function addItem(e) {
+    
+}
